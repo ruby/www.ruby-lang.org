94a796bef183a564e7070d077c70c71208d9cd86
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-04-01 12:07:10 +0900

    remove shell environament variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,7 @@ end
 
 desc "Generate static sites."
 task :generate_static_sites => :environment do
-  queue 'echo $LANG; echo $LC_ALL; ruby -v'
-  queue 'LANG=ja_JP.UTF-8; LC_ALL=ja_JP.UTF-8; bundle exec rake generate'
+  queue 'bundle exec rake generate'
 end
 
 desc "Deploys the current version to the server."
