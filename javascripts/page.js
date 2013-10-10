eaa19f8699c6b0b151948173a0c79fdaee08c97c
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2013-10-09 21:53:46 -0300

    Added script

--- javascripts/page.js
+++ javascripts/page.js
@@ -1,5 +1,15 @@
 var Page = {
+  SiteLinks: {
+    highlight: function() {
+      var current_page = location.pathname;
 
+      $("div.site-links a").each(function(i) {
+        if (current_page.indexOf($(this).attr('href')) == 0) {
+          $(this).css('font-weight', 'bold');
+        }
+      });
+    }
+  }
 };
 
 $(Page.SiteLinks.highlight);
