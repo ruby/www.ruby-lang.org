50da2a3623e642af7ee5de93eb64990afdabeb7a
Marcus Stollsteimer <sto.mar@web.de>
2020-12-27 23:54:49 +0100

    Introduce methods for locales, month_names

--- _plugins/news.rb
+++ _plugins/news.rb
@@ -6,6 +6,8 @@ module Jekyll
   module News
     class ArchivePage < Page
 
+      attr_reader :lang
+
       def initialize(site, base, subdir, layout, lang, posts)
         @site = site
         @base = base
@@ -16,12 +18,7 @@ module Jekyll
                 end
         @name = "index.html"
 
-        @locales = site.data["locales"][lang]["news"] ||
-                   site.data["locales"]["en"]["news"]
-
-        @month_names = site.data["locales"][lang]["month_names"] ||
-                       site.data["locales"]["en"]["month_names"]
-        @month_names = ["None"] + @month_names
+        @lang = lang
 
         process(@name)
         read_yaml(File.join(base, "_layouts"), layout)
@@ -34,12 +31,22 @@ module Jekyll
         "news"
       end
 
+      def locales
+        site.data["locales"][lang]["news"] ||
+          site.data["locales"]["en"]["news"]
+      end
+
+      def month_names
+        ["None"] + (site.data["locales"][lang]["month_names"] ||
+                    site.data["locales"]["en"]["month_names"])
+      end
+
       def insert_date(string, year, month = 0)
         substitutions = {
           "%Y" => year.to_s,
           "%m" => "%.2d" % month,
           "%-m" => month.to_s,
-          "%B" => @month_names[month]
+          "%B" => month_names[month]
         }
 
         string.gsub(/%Y|%m|%-m|%B/, substitutions)
@@ -55,7 +62,7 @@ module Jekyll
 
         super(site, base, subdir, LAYOUT, lang, posts)
 
-        title = @locales["monthly_archive_title"]
+        title = locales["monthly_archive_title"]
 
         data["title"] = insert_date(title, year, month)
         data["year"]  = year
@@ -71,13 +78,13 @@ module Jekyll
 
         super(site, base, subdir, LAYOUT, lang, posts)
 
-        title = @locales["yearly_archive_title"]
+        title = locales["yearly_archive_title"]
 
         data["title"] = insert_date(title, year)
         data["year"]  = year
 
         months = posts.map {|post| post.date.month }.uniq
-        month_link_text = @locales["monthly_archive_link"]
+        month_link_text = locales["monthly_archive_link"]
 
         # hash with url => link_text (including year) elements
         data["months"] = Hash[
@@ -98,13 +105,13 @@ module Jekyll
         subdir = nil
         super(site, base, subdir, LAYOUT, lang, posts)
 
-        title = @locales["recent_news"]
+        title = locales["recent_news"]
 
         data["title"] = title
         data["posts"] = posts.last(MAX_POSTS).reverse
 
         years = posts.map {|post| post.date.year }.uniq.reverse
-        year_link_text = @locales["yearly_archive_link"]
+        year_link_text = locales["yearly_archive_link"]
 
         # hash with url => link_text elements
         data["years"] = Hash[
