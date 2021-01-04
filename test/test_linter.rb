9fba403e597a53a090455c93924599ce387c02d6
Marcus Stollsteimer <sto.mar@web.de>
2021-01-04 20:54:32 +0100

    Cleanup old Linter tests for valid pages/posts

--- test/test_linter.rb
+++ test/test_linter.rb
@@ -10,18 +10,9 @@ FIXTURES_PATH = "#{SRCPATH}/fixtures"
 describe Linter do
 
   before do
-    @md_ok = "#{FIXTURES_PATH}/ok"
     @md_errors = "#{FIXTURES_PATH}/errors"
 
-    @output_ok = File.read("#{FIXTURES_PATH}/output_ok.txt")
     @output_errors = File.read("#{FIXTURES_PATH}/output_errors.txt")
-  end
-
-  it "can run ok" do
-    Dir.chdir @md_ok
-    stdout, _stderr = capture_io { Linter.new(exit_on_errors: false).run }
-
-    _(stdout).must_equal @output_ok
   end
 
   it "can find errors" do
