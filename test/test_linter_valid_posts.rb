0cbe1c5145f6b1f8123ba1acddb8f3d0b677330a
Marcus Stollsteimer <sto.mar@web.de>
2021-01-04 20:54:28 +0100

    Migrate Linter test cases for valid posts with SHA sums

--- test/test_linter_valid_posts.rb
+++ test/test_linter_valid_posts.rb
@@ -68,4 +68,75 @@ describe Linter do
     create_file("en/news/_posts/2000-01-01-old-without-translator.md", content)
     _(linter_output).must_equal @ok
   end
+
+  it "checks ok a valid release post with SHA sums" do
+    content = <<~POST
+      ---
+      layout: news_post
+      title: "Release Post"
+      author: "stomar"
+      translator:
+      date: 2019-01-01 12:00:00 +0000
+      lang: en
+      ---
+
+      Content
+
+      Sums:
+
+          SHA1:    abcdef7890123456789012345678901234567890
+          SHA256:  abcdef7890123456789012345678901234567890123456789012345678901234
+          SHA512:  abcdef78901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678
+    POST
+
+    create_file("en/news/_posts/2019-01-01-ruby-with-sha-sums-released.md", content)
+    _(linter_output).must_equal @ok
+  end
+
+  it "checks ok a valid release post with SHA sums as liquid tags" do
+    content = <<~POST
+      ---
+      layout: news_post
+      title: "Release Post"
+      author: "stomar"
+      translator:
+      date: 2019-01-01 12:00:00 +0000
+      lang: en
+      ---
+
+      Content
+
+      Sums:
+
+          SHA1:    {{ sha1 }}
+          SHA256:  {{ sha256 }}
+          SHA512:  {{ sha512 }}
+    POST
+
+    create_file("en/news/_posts/2019-01-01-ruby-with-sha-sums-liquid-released.md", content)
+    _(linter_output).must_equal @ok
+  end
+
+  it "checks ok a valid old release post with missing SHA512 sum" do
+    content = <<~POST
+      ---
+      layout: news_post
+      title: "Release Post"
+      author: "stomar"
+      translator:
+      date: 2019-01-01 12:00:00 +0000
+      lang: en
+      ---
+
+      Content
+
+      Sums:
+
+          SHA1:    abcdef7890123456789012345678901234567890
+          SHA256:  abcdef7890123456789012345678901234567890123456789012345678901234
+    POST
+
+    create_file("en/news/_posts/2019-01-01-ruby-without-sha512-sum-released.md", content)
+    _(linter_output).must_equal @ok
+  end
 end
