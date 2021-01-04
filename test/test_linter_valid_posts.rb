62cd5f67eec6c4bfae0907b4f3c5adb808073e11
Marcus Stollsteimer <sto.mar@web.de>
2021-01-04 20:54:22 +0100

    Migrate Linter test case for valid post

---
+++ test/test_linter_valid_posts.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "helper"
+require "linter"
+
+
+describe Linter do
+
+  before do
+    chdir_tempdir
+    create_releases_file
+
+    @ok = "Checking markdown files... ok\n"
+  end
+
+  after do
+    teardown_tempdir
+  end
+
+  it "checks ok a valid post" do
+    content = <<~POST
+      ---
+      layout: news_post
+      title: "Post"
+      author: "stomar"
+      translator:
+      date: 2018-01-01 12:00:00 +0000
+      lang: en
+      ---
+
+      Content
+    POST
+
+    create_file("en/news/_posts/2018-01-01-post.md", content)
+    _(linter_output).must_equal @ok
+  end
+end
