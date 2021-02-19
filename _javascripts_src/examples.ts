fe098480aa65fdddff92142018207abfd1023627
Marcus Stollsteimer <sto.mar@web.de>
2021-02-19 17:34:15 +0100

    Introduce TypeScript
    Set up TypeScript (add source directory and config file).
    The JavaScript files in `javascripts` can now be generated
    by running `tsc` in the root directory.
    
    This might be useful for future changes of the JavaScript.
    It does not affect the normal build process of the site.

---
+++ _javascripts_src/examples.ts
@@ -0,0 +1,16 @@
+var Examples = {
+    names: ['cities', 'greeter', 'i_love_ruby', 'hello_world'],
+
+    random: function () {
+        return Examples.names[Math.floor(Math.random() * Examples.names.length)];
+    },
+
+    choose: function () {
+        var lang = document.location.pathname.split('/')[1];
+        var name = Examples.random();
+
+        $("#code").load('/' + lang + '/examples/' + name + '/');
+    }
+};
+
+$(document).ready(Examples.choose);
