eea7718405c15c22c9076f408c19e7330a8ad2ee
Marcus Stollsteimer <sto.mar@web.de>
2021-02-19 17:25:09 +0100

    Reformat JavaScript files
    Reformat JavaScript files to match code generated by TypeScript,
    in preparation for the introduction of TypeScript files.

--- javascripts/page.js
+++ javascripts/page.js
@@ -1,24 +1,21 @@
 var Page = {
-  SiteLinks: {
-    highlight: function() {
-      var current_page = location.pathname;
-      $("#header div.site-links a:not(.home)").each(function(i) {
-        if (current_page.indexOf($(this).attr('href')) == 0) {
-          $(this).addClass('selected');
+    SiteLinks: {
+        highlight: function () {
+            var current_page = location.pathname;
+            $("#header div.site-links a:not(.home)").each(function (i) {
+                if (current_page.indexOf($(this).attr('href')) == 0) {
+                    $(this).addClass('selected');
+                }
+            });
+            $("#home-page-layout #header div.site-links a.home").addClass('selected');
+        },
+        menu: function () {
+            $("#header div.site-links a.menu").on('click touchstart', function (event) {
+                $(this).closest("div.site-links").toggleClass("open");
+                event.preventDefault();
+            });
         }
-      });
-
-      $("#home-page-layout #header div.site-links a.home").addClass('selected');
-    },
-
-    menu: function() {
-      $("#header div.site-links a.menu").on('click touchstart', function(event) {
-        $(this).closest("div.site-links").toggleClass("open");
-        event.preventDefault();
-      });
     }
-  }
 };
-
 $(Page.SiteLinks.highlight);
 $(Page.SiteLinks.menu);
