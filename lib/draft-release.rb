1513a94e0c40fa8ac376c0222f7774c704c6645f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-09-04 14:15:38 +0900

    Added misisng ruby- prefix

--- lib/draft-release.rb
+++ lib/draft-release.rb
@@ -3,7 +3,7 @@ require "erb"
 template = File.read("lib/release-template.erb")
 version = ARGV[0] || "3.3.4"
 
-output_path = "en/news/_posts/#{Time.now.strftime("%Y-%m-%d")}-#{version.gsub(/\./, "-")}-released.md"
+output_path = "en/news/_posts/ruby-#{Time.now.strftime("%Y-%m-%d")}-#{version.gsub(/\./, "-")}-released.md"
 
 File.open(output_path, "w") do |file|
   file.write ERB.new(template).result(binding)
