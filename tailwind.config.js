48040fd3642dcaf783503237f1cd6b816c775497
taea <igiari@gmail.com>
2025-10-02 13:20:37 +0900

    Use Tailwind default

--- tailwind.config.js
+++ tailwind.config.js
@@ -122,71 +122,15 @@ module.exports = {
         'code': ['Courier', 'ui-monospace', 'SFMono-Regular', 'Consolas', 'monospace'],
         'icon': ['"Material Icons"', 'sans-serif'],
       },
-      spacing: {
-        '1': '0.3125rem',   // 5px
-        '1.5': '0.375rem',  // 6px
-        '2': '0.5rem',      // 8px
-        '3': '0.75rem',     // 12px
-        '4': '0.9375rem',   // 15px
-        '5': '1.25rem',     // 20px
-        '6': '1.5rem',      // 24px
-        '8': '1.875rem',    // 30px
-        '10': '2.5rem',     // 40px
-        '12': '3rem',       // 48px
-        '15': '3.75rem',    // 60px
-        '20': '5rem',       // 80px
-      },
-      borderRadius: {
-        'sm': '0.1875rem',  // 3px
-        'DEFAULT': '0.375rem', // 6px
-        'lg': '0.375rem',   // 6px
-      },
-      boxShadow: {
-        'sm': '0 1px 2px #ddd inset',
-        'DEFAULT': '0 0 3px rgba(0,0,0,0.4)',
-        'md': '0 2px 0 #c2c2b8',
-        'lg': '0 2px 5px rgba(0,0,0,0.5)',
-        'inset': '0 0 4px rgba(0,0,0,0.4) inset',
-        'button': '0 2px 0 #973329',
-        'button-hover': '0 2px 0 #c2c2b8',
-        'download': '0 2px 0 #9b1c16',
-      },
       maxWidth: {
         'container': '61.25rem',  // 980px
         'content': '70%',
         'sidebar': '30%',
         'code': '23.4375rem',     // 375px
         'search': '11.25rem',      // 180px
-      },
-      gridTemplateColumns: {
-        'layout': '70% 30%',
-        'intro': '1fr 23.4375rem',  // 1fr 375px
-      },
-      transitionProperty: {
-        'opacity': 'opacity',
-        'colors': 'color, background-color, border-color',
-      },
-      textShadow: {
-        'sm': '0 1px 0 #fff',
-        'dark': '0 1px 0 #000',
       }
     },
   },
-  plugins: [
-    function({ addUtilities }) {
-      const newUtilities = {
-        '.text-shadow-sm': {
-          textShadow: '0 1px 0 #fff',
-        },
-        '.text-shadow-dark': {
-          textShadow: '0 1px 0 #000',
-        },
-        '.text-shadow-none': {
-          textShadow: 'none',
-        },
-      }
-      addUtilities(newUtilities)
-    }
-  ],
+  plugins: [],
 }
 
