ec738a9de7ac90aa7bc0f70e44efcd9f2058ea27
user <ssharp0614@gmail.com>
2026-01-01 12:02:51 +0900

    Refactor stylesheet structure: move components to `_components` and update paths, add `postcss-import` to manage imports more efficiently.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+const path = require('node:path');
+
 module.exports = {
   plugins: {
+    "postcss-import": {
+      path: path.join(__dirname, 'stylesheets')
+    },
     tailwindcss: {},
     autoprefixer: {},
   }
-}
+};
